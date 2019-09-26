a8cbd9f986d66c3705e02102d52cd5dcae2a986b
francesca.boe <francesca.boe@accenture.com>
2019-09-26 16:18:43 +0200

    fixed navigator in App

--- App.js
+++ App.js
@@ -1,25 +1,77 @@
 import React from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Constants} from "expo";
 
-import {Constants} from "expo"; //System information that remains constant throughout the lifetime of your app.
-import {TabNavigator} from "react-navigation";
+import {Image, Platform, View} from 'react-native';
+import {createAppContainer, createBottomTabNavigator} from 'react-navigation';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
-    const platformOS = Platform.OS.toLowerCase(); //tells us what platform we are running on
-    console.log("Your application is running on", platformOS)
-    return (
-        <View style={styles.container}>
-            <Text>RestaurantChooser App</Text>
-        </View>
-    );
-}
+import {HomeScreen} from "./screens/HomeScreen";
+import {PeopleScreen} from "./screens/PeopleScreen";
+import {DecisionScreen} from "./screens/DecisionScreen";
+import {RestaurantsScreen} from "./screens/RestaurantsScreen";
 
+console.log("yo Your application is running on", Platform.OS.toLowerCase());//tells us what platform we are running on
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+//NAVIGATOR
+const TabNavigator = createMaterialBottomTabNavigator(
+    { //routes
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                tabBarLabel: 'Home',
+                tabBarIcon: ({tintColor}) => (
+                    <View>
+                        <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>
+                    </View>),
+            }
+        },
+        People: {
+            screen: PeopleScreen,
+            navigationOptions: {
+                tabBarLabel: 'People',
+                tabBarIcon: ({tintColor}) => (
+                    <Image source={require("./images/icon-people.png")}
+                           style={{width: 32, height: 32, tintColor: tintColor}}/>
+                ),
+                activeColor: '#f60c0d',
+                inactiveColor: '#f65a22',
+                barStyle: {backgroundColor: '#f69b31'},
+            }
+        },
+        Decision: {
+            screen: DecisionScreen,
+            navigationOptions: {
+                tabBarLabel: 'Decision',
+                tabBarIcon: ({tintColor}) => (
+                    <Image source={require("./images/icon-decision.png")}
+                           style={{width: 32, height: 32, tintColor: tintColor}}/>
+                ),
+                activeColor: '#615af6',
+                inactiveColor: '#46f6d7',
+                barStyle: {backgroundColor: '#67baf6'},
+            }
+        },
+        Restaurants: {
+            screen: RestaurantsScreen,
+            navigationOptions: {
+                tabBarLabel: 'Restaurants',
+                tabBarIcon: ({tintColor}) => (
+                    <Image source={require("./images/icon-restaurants.png")}
+                           style={{width: 32, height: 32, tintColor: tintColor}}
+                    />),
+                activeColor: '#0b5fff',
+                inactiveColor: '#699bff',
+                barStyle: {backgroundColor: '#b5c8f6'},
+            }
+        },
     },
-});
+    { //configurations
+        initialRouteName: "Home",
+        activeColor: '#f0edf6', //default if not chosen otherwise
+        inactiveColor: '#226557',
+        barStyle: {backgroundColor: '#3BAD87'},
+    },
+);
+
+export default createAppContainer(TabNavigator); //
